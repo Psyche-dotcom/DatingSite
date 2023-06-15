4a86ba5299cbaf4b4730c4650d50e99682f76c38
Joshodey <joshodey@live.com>
2023-06-15 21:41:49 +0100

    Payment service configured

--- Backend/Model/DTO/SignUp.cs
+++ Backend/Model/DTO/SignUp.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Model.DTO
 {
-    public class SignUp
+    public class SignUp : SignInModel
     {
         [Required]
         public string FirstName { get; set; }
@@ -11,15 +11,15 @@ namespace Model.DTO
         public string LastName { get; set; }
         public string Location { get; set; }
         public int Age { get; set; }
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        //[Required]
+        //[EmailAddress]
+        //public string Email { get; set; }
         [Required]
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; } = string.Empty;
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+        //[Required]
+        //[DataType(DataType.Password)]
+        //public string Password { get; set; }
         [Required]
         [Compare("Password")]
         [DataType(DataType.Password)]
--- Backend/Model/Enitities/Payments.cs
+++ Backend/Model/Enitities/Payments.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,17 @@ namespace Model.Enitities
 {
     public class Payments
     {
+        [Key]
         public string Id { get; set; }
         public string Amount { get; set; }
         public string ReferenceNumber { get; set; }
+        public string Description { get; set; }
         public string PaymentType { get; set; }
+        public DateTime PaymentTime { get; set; }
+        public bool IsActive { get; set; }
+        
+        [ForeignKey(nameof(ApplicationUser))]
         public string UserId { get; set; }
-
         public ApplicationUser User { get; set; }
     }
 }
